d42fd8fd916d43d7285cbbc42778e70a4a962b14
Nikita <akter.kina@gmail.com>
2024-09-26 18:04:29 +0300

    ''

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.scss";
-import store from "./store";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <HashRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </HashRouter>,
 );
