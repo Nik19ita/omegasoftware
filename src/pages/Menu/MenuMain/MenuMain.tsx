fbaa6965d375f110b37570a915f86e44a70dbc47
Nikita <akter.kina@gmail.com>
2024-09-26 19:03:37 +0300

    ''

--- src/pages/Menu/MenuMain/MenuMain.tsx
+++ src/pages/Menu/MenuMain/MenuMain.tsx
@@ -8,7 +8,7 @@ const MenuPages = () => {
   return (
     <Content>
       <List data={data} listName={`${location.key}`} />
-      <img src="./omegasoftware/src/assets/img/test.jpg" alt="sdg" />
+      <img src="src/assets/img/test.jpg" alt="sdg" />
     </Content>
   );
 };
